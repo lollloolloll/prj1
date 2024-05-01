1405f135abb176be28ff1f3e7e87f2f0ec2fbb2f
ganghyeon jo <ganghyeon4479@naver.com>
2024-05-01 09:47:11 +0900

    설정 완료

---
+++ sql/prj1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE prj1;
+
+USE prj1;
+
+DROP TABLE board;
+CREATE TABLE board
+(
+    ID       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(200)  NOT NULL,
+    content  VARCHAR(3000) NOT NULL,
+    wirter   VARCHAR(100)  NOT NULL,
+    inserted DATETIME DEFAULT NOW()
+);
+
